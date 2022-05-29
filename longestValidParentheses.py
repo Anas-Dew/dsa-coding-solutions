394b2e9339d615f08bfd5a76aac586e08a74a9c4
anas-dew <mailtoanas@duck.com>
2022-05-29 17:38:27 +0530

    Create longestValidParentheses.py

---
+++ longestValidParentheses.py
@@ -0,0 +1,16 @@
+def solve(S: str):
+        stack, ans = [-1], 0
+        for i in range(len(S)):
+            if S[i] == '(': stack.append(i)
+            elif len(stack) == 1: stack[0] = i
+            else:
+                stack.pop()
+                ans = max(ans, i - stack[-1])
+        return ans
+
+
+
+test_cases = ['((()', ')()())', '(((((', '(((()()))())', ')))))(']
+for i in test_cases:
+    print(solve(i), end=' ')
+
