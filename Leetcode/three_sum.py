fee95e6c5cca8fd6db81ffb39128787194a9a7c8
anas-dew <mailtoanas@duck.com>
2023-01-01 13:27:10 +0530

    1 JAN

--- Leetcode/three_sum.py
+++ Leetcode/three_sum.py
@@ -1,19 +1,37 @@
 class Solution:
-    def summer(self, triplets):
-        return sum(triplets)
 
     def threeSum(self, nums):
+        """
+        We sort the array, then iterate through the array, and for each element, we use two pointers to
+        find the other two elements that sum to the target
+        
+        :param nums: the list of numbers
+        :return: A list of lists of integers.
+        """
         ans = []
-        for i in range(len(nums)):
-            for j in range(i, len(nums)):
-                for k in range(j, len(nums)):
-                    trips = [nums[i], nums[j], nums[k]]
-                    if self.summer(trips) == 0:
-                        if j == 1 : ans.append(trips)
-                        if len(ans) > 0 == True:
-                            if trips != ans[-1]:
-                                ans.append(trips)
+        nums.sort()
+
+        for i, a in enumerate(nums):
+           # This is to avoid duplicates.
+            if i > 0 and a == nums[i-1]:
+                continue
+
+            l, r = i + 1, len(nums) - 1
+
+            while l < r:
+                threesum = a + nums[l] + nums[r]
+                if threesum > 0:
+                    r -= 1
+                elif threesum < 0:
+                    l += 1
+                else :
+                    ans.append([a, nums[l], nums[r]])
+                    l += 1
+                    # This is to avoid duplicates.
+                    while nums[l] == nums[l - 1] and l < r :
+                        l += 1
         return ans
+
 
 a = [-1,0,1,2,-1,-4]
 ans = Solution()
